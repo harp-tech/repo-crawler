dd7e2f8e06608569f8e109a089b2bea4f8e3dae0
bruno-f-cruz <bfpcruz@gmail.com>
2023-01-06 11:04:26 +0000

    Update to include peripherals and template repos

--- harpcrawler/harprepo/__init__.py
+++ harpcrawler/harprepo/__init__.py
@@ -1,14 +1,6 @@
 from github import Github, Repository
 from enum import Enum
 
-
-class DeviceFiles(Enum):
-    README = "README.md"
-    GITIGNORE = ".gitignore"
-    FIRMWARE_GITIGNORE = "Firmware/.gitignore"
-    HARDWARE_GITIGNORE = "Hardware/.gitignore"
-    INTERFACES_GITIGNORE = "Interfaces/.gitignore"
-
 class RepositoryType(Enum):
     GENERIC = "GENERIC"
     PERIPHERAL = "Peripheral"
@@ -19,7 +11,7 @@ class HarpRepo():
         repository: Repository.Repository) -> None:
 
         self.repository = repository
-        self.filetree = []
+        self.filetree = None
         self.repository_type = RepositoryType.GENERIC
 
     def populate_repo(self):
@@ -32,11 +24,9 @@ class HarpRepo():
             else:
                 self.filetree.append(file_content.path)
 
-    def get_readme(self, opt_path = DeviceFiles.README):
-        file = self.repository.get_contents(opt_path)
-        return file
-
     def exist_harpfiles(self, path_list):
+        if self.filetree is None:
+            self.populate_repo()
         return ({file : file in self.filetree for file in path_list})
 
     def __repr__(self) -> str:
@@ -44,21 +34,6 @@ class HarpRepo():
     def __str__(self) -> str:
         return(str(self.repository))
 
-class DeviceRepo(HarpRepo):
 
-    def __init__(self, repository: Repository.Repository) -> None:
-        super().__init__(repository=repository)
-        self.repository_type = RepositoryType.DEVICE
 
-    def exist_harpfiles(self, path_list = None):
-        if path_list is None:
-            return ({file : file.value in self.filetree for file in DeviceFiles})
-        else:
-            return (super().exist_harpfiles(path_list))
-
-class PeripheralRepo(HarpRepo):
-
-    def __init__(self, repository: Repository.Repository) -> None:
-        super().__init__(repository=repository)
-        self.repository_type = RepositoryType.PERIPHERAL
 
---
+++ harpcrawler/harprepo/peripheral.py
@@ -0,0 +1,24 @@
+from github import Github, Repository
+from harprepo import HarpRepo, RepositoryType
+
+class PeripheralRepo(HarpRepo):
+
+    def __init__(self,
+    repository: Repository.Repository,
+    template: Repository.Repository | None = None) -> None:
+        super().__init__(repository=repository)
+        self.repository_type = RepositoryType.PERIPHERAL
+        self.template = template
+
+    def exist_harpfiles(self, path_list: list = None):
+        if path_list is None:
+            if self.template is not None:
+                path_list = self.template.filetree
+            else:
+                raise ValueError("A valid template target must be provided!")
+        return (super().exist_harpfiles(path_list))
+
+class TemplatePeripheralRepo(PeripheralRepo):
+        def __init__(self, repository: Repository.Repository) -> None:
+            super().__init__(repository=repository)
+            self.populate_repo()
