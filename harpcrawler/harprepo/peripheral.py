fdc2df74a4c2e3f3afbe2f49df6f829242e76c46
bruno-f-cruz <bfpcruz@gmail.com>
2023-01-10 16:43:30 +0000

    add diagnosis repository table to template classes

--- harpcrawler/harprepo/peripheral.py
+++ harpcrawler/harprepo/peripheral.py
@@ -1,5 +1,8 @@
-from github import Github, Repository
+import pandas as pd
+
+from github import Repository
 from harprepo import HarpRepo, RepositoryType
+from typing import List
 
 class PeripheralRepo(HarpRepo):
 
@@ -22,3 +25,30 @@ class TemplatePeripheralRepo(PeripheralRepo):
         def __init__(self, repository: Repository.Repository) -> None:
             super().__init__(repository=repository)
             self.populate_repo()
+            self.diagnosis_table = None
+
+        def run_diagnosis(self, repos_to_validate: List[PeripheralRepo]) -> None:
+            diagnosis_table = pd.DataFrame(columns = self.filetree)
+            diagnosis_table = diagnosis_table.astype('bool')
+
+            for repo in repos_to_validate:
+                diagnosis_table = pd.concat([diagnosis_table,\
+                    pd.DataFrame(repo.exist_harpfiles(), index = [repo.repository.full_name.split("/")[-1]])\
+                        ], axis=0, ignore_index=False)
+            self.diagnosis_table = diagnosis_table
+
+        def print_diagnosis(self) -> pd.DataFrame:
+            if self.diagnosis_table is None:
+                raise ValueError("A valid diagnosis table does not exist. \
+                    Try to use the run_diagnosis() method first.")
+            else:
+                diagnosis_table = self.diagnosis_table.copy()
+                cols = diagnosis_table.columns.to_list()
+                cols.insert(0,'Device')
+                diagnosis_table['Device'] = diagnosis_table.index
+                diagnosis_table = diagnosis_table[cols]
+                diagnosis_table = diagnosis_table.replace({True: '\u2705', False: '\u274C'})
+                return diagnosis_table
+
+
+
