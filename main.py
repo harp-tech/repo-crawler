23a651fc999ca4cf69e418917100340706d5eb1e
bruno-f-cruz <bfpcruz@gmail.com>
2023-03-28 14:53:03 +0100

    Add entry point for github actions

---
+++ main.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import pandas as pd
+import json
+
+from github import Github
+
+from harpcrawler.harprepo.device import DeviceRepo, TemplateDeviceRepo
+from harpcrawler.harprepo.peripheral import PeripheralRepo, TemplatePeripheralRepo
+from harpcrawler.gsheets import HarpSpreadsheet
+
+# Tokens
+GITHUB_KEY = os.envrion.get("CREDENTIAL_GITHUB", None)
+GOOGLE_KEY = os.envrion.get("CREDENTIAL_GOOGLE", None)
+
+# Flags
+ORGANIZATION = "harp-tech"
+SPREADSHEET = "GitHarpCrawler"
+PRINT_DIAGNOSIS = True
+UPDATE_SPREADSHEET = False
+
+
+def main():
+    # Setup
+    if UPDATE_SPREADSHEET:
+        harpSpreadsheet = HarpSpreadsheet(spreadsheet=SPREADSHEET,
+                                          credentials=GOOGLE_KEY)
+
+    gh = Github(login_or_token=GITHUB_KEY)
+
+    device_template = TemplateDeviceRepo(gh.get_repo(f"{ORGANIZATION}/device.template"))
+    peripheral_template = TemplatePeripheralRepo(gh.get_repo(f"{ORGANIZATION}/peripheral.template"))
+
+    harp_organization = gh.get_organization(ORGANIZATION)
+
+    # Get all "Device.*" repositories
+    device_repos = [DeviceRepo(repo, device_template)
+                    for repo in harp_organization.get_repos()
+                    if (("device." in repo.full_name) and
+                        not("template" in repo.full_name) and
+                        not(repo.is_template))]
+
+    device_template.run_diagnosis(repos_to_validate=device_repos)
+    device_diagnosis = device_template.print_diagnosis().applymap(lambda x: str(x))
+
+    if PRINT_DIAGNOSIS:
+        print(device_diagnosis)
+    if UPDATE_SPREADSHEET:
+        harpSpreadsheet.open_spreadsheet()
+        harpSpreadsheet.update_spreadsheet("devices", device_diagnosis)
+
+    # Get all "Peripherals.*" repositories
+
+    peripheral_repos = [PeripheralRepo(repo, peripheral_template)
+                        for repo in harp_organization.get_repos()
+                        if (("peripheral." in repo.full_name) and
+                            not("template" in repo.full_name) and
+                            not(repo.is_template))]
+    peripheral_template.run_diagnosis(repos_to_validate=peripheral_repos)
+    peripherals_diagnosis = peripheral_template.print_diagnosis().applymap(lambda x: str(x))
+
+    if PRINT_DIAGNOSIS:
+        print(peripherals_diagnosis)
+    if UPDATE_SPREADSHEET:
+        harpSpreadsheet.open_spreadsheet()
+        harpSpreadsheet.update_spreadsheet("peripherals", peripherals_diagnosis)
+
+    return 0
+
+
+if __name__ == "__main__":
+    main()
